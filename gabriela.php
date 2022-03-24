af624dfd0716223fb173847efb61ffeb8d766311
GABRIELA123324242 <101895974+GABRIELA123324242@users.noreply.github.com>
2022-03-24 17:30:23 -0500

    gabriela alejandra camacho torres 1102

---
+++ gabriela.php
@@ -0,0 +1,9 @@
+
+<?php
+//definimo una variable con un texto 
+$mVariable = "hola variable"
+echo "<br>";
+$miValor = 2;
+//cisualizamos sin embargo realizamos una multiplicacion * 3 
+echo 3*$miValor
+?>
